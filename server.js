4301d453eadaed64fa097005d752a7c38a35f112
mulesiddhi <mulesiddhi@gmail.com>
2022-04-27 13:33:26 +0530

    db

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const {check, validationResult} = require('express-validator');
 const app = express();
+
 
 const mongoose=require('mongoose');
 const session = require('express-session');
-var findOrCreate = require('mongoose-findorcreate')
+var findOrCreate = require('mongoose-findorcreate');
+const { name } = require('ejs');
 
 const server='127.0.0.1:27017';
 const db='foodogramDB';
@@ -50,22 +54,71 @@ app.listen(3000, function() {
 app.get('/',(req,res)=>{
     res.render('login');  
 })
+app.post('/',(req,res)=>{
+
+    const email=req.body.email;
+    const password=req.body.password;
+
+    User.find({email:email}).then(users=>{
+            // console.log(users);
+            if(users.length===0){
+                res.render('login',{message:'email does not exist'});
+            }else{
+                if(users[0].password===password){
+                    res.render('home',{user:users[0]});
+                }else{
+                    res.render('login',{message:'password is incorrect'});
+                }
+            }
+        }).catch(err=>{
+            console.log(err);
+        });  
+})
 //signup
 app.get('/signup',(req,res)=>{
     res.render('signUp');  
 })
 
-app.post('/signup',(req,res)=>{
-  const user=new User({
-      name:req.body.name,
-      username:req.body.username,
-      address:req.body.address,
-      email:req.body.email,
-      password:req.body.password,
-      gender:req.body.gender  
-})
+app.post('/signup',[
+    
+    check('email','Invalid Email').isEmail(),
+    check('confirmPassword', 'Passwords do not match').custom(( value,{req}) => (req.body.confirmpassword !== req.body.password)),
+    check('name','Name is required').not().isEmpty(),
+    check('username','Username is required').not().isEmpty(),
+    check('address','Address is required').not().isEmpty(),
+    check('password','Password is required').not().isEmpty(),
+    check('gender','gender field required').not().isEmpty()
 
-})
+],(req,res)=>{
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        const alert=errors.array();
+        res.render('signUp',{alert});
+    }
+    else{
+        const user=new User({
+            name:req.body.name,
+            username:req.body.username,
+            address:req.body.address,
+            email:req.body.email,
+            password:req.body.password,
+            gender:req.body.gender  
+      })
+      
+      user.save(function(err){
+                  if(err){
+                      console.log(err);
+                  }else{
+                      res.render('home');
+                  }
+              })
+      
+    }
+
+
+
+
+});
 
 //home
 app.get('/home',(req,res)=>{
