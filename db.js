03e85051422b9abc3c14d8ae91376eea67c7707e
hetul <hetul.mehta@somaiya.edu>
2022-04-28 20:28:48 +0530

    env added

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-
-const url = `mongodb+srv://hetul:hello@project.8prig.mongodb.net/foodogram?retryWrites=true&w=majority`;
+const env=require('dotenv').config();
+const url = process.env.url;
 async function dbConnect() {
   // use mongoose to connect this app to our database on mongoDB using the DB_URL (connection string)
   mongoose
