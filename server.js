e12e2be5f8653a8300b6c4ccda65051186184007
mulesiddhi <mulesiddhi@gmail.com>
2022-04-30 22:40:38 +0530

    createpost

--- server.js
+++ server.js
@@ -226,7 +226,7 @@ app.get("/findmybuddy", (req, res) => {
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./uploads");
+    cb(null, "./public/uploads");
   },
   filename: function (req, file, cb) {
     cb(
