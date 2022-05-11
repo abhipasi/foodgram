4b8eb816a359eda8284c178ba93447dcb8b5cd26
hetul <hetul.mehta@somaiya.edu>
2022-05-11 18:52:25 +0530

    p

--- classification/predict.py
+++ classification/predict.py
@@ -8,8 +8,7 @@ INIT_LR = 1e-6
 IMG_WIDTH, IMG_HEIGHT = 299, 299 
 label={0: 'Bread', 1: 'Dairy product', 2: 'Dessert', 3: 'Egg', 4: 'Fried food', 5: 'Meat', 6: 'Noodles-Pasta', 7: 'Rice', 
        8: 'Seafood', 9: 'Soup', 10: 'Vegetable-Fruit'}
-img_path="C:/Users/shrujal/Downloads/download.jpg"
-# sys.argv[1]
+img_path=sys.argv[1]
 model = load_model('C:/Users/shrujal/OneDrive/Desktop/foodogramNode/classification/inceptionv3_multiclass_best.h5')
 model.compile(optimizer=tf.keras.optimizers.Adam(lr=INIT_LR), loss='categorical_crossentropy', metrics=['categorical_accuracy'])
 def load_image(img_path):
